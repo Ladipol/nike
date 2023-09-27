8668b9208f79b350a5e46deb056107a96eadca10
Ladipo Layode <ladipol@gmail.com>
2023-09-27 18:08:01 +0100

    change h1 to h2

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -6,14 +6,13 @@ const SuperQuality = () => {
       id="about-us"
       className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container">
       <div className="flex flex-1 flex-col">
-        <img src={} alt="" />
-        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:text-[82] font-bold">
+        <h2 className="font-palanquin text-8xl max-sm:text-[72px] max-sm:text-[82] font-bold">
           <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
             The New Arrival
           </span>
           <br />
           <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
-        </h1>
+        </h2>
         <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
           Discover stylish Nike arrivals, quality comfort, and innovation for
           your active life.
